4deca1bb12f8058789bb745418cd06315ce472a0
Gustavo Herrero Nunes <51753426+GustavoHerreroNunes@users.noreply.github.com>
2024-04-18 18:14:46 +0000

    Add: Kotlin build test files

---
+++ main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val name = "Gustavo"
+    var nickname = "ghn"
+
+    if(name == nickname)
+        println("I know you can be more creative than that, came'on")
+    else
+        println("Cool nickname, guy!")
+}
