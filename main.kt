91b1b5ddf549f7d3d9aea58813aacb9cac925d86
GustavoHerreroNunes <ghnunes2010@gmail.com>
2024-04-19 09:32:37 -0300

    Add: New branch with modern android studio project structure

--- main.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val name = "Gustavo"
-    var nickname = "ghn"
-
-    if(name == nickname)
-        println("I know you can be more creative than that, came'on")
-    else
-        println("Cool nickname, guy!")
-}
